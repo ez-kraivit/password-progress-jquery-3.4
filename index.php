88d82c3f63652dc4f7ea770031e5dc7e130afa47
Kraivit Mongkhonsakunrit <kraivit@edvisory.co.th>
2020-05-08 15:36:10 +0700

    Update jquery 3.4.0

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password </title>
+    <script src="https://code.jquery.com/jquery-3.4.0.js"
+        integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
+    <script src="password.js"></script>
+</head>
+
+<body>
+    <div class="container mt-3">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" id="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped" role="progressbar" id="progressbar" aria-valuenow="0"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <button type="submit" id="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ password.js
@@ -0,0 +1,67 @@
+jQuery(function () {
+    $("#submit").prop('disabled', true);
+    $("#password").keyup(function () {
+        var pwd = $("#password").val();
+        progressbar(pwd);
+        if (pwd.length > 7) {
+            $("#submit").prop('disabled', false);
+        } else {
+            $("#submit").prop('disabled', true);
+        }
+    });
+
+    function progressbar(pwd) {
+        var checkstrength = 0;
+        if (pwd.match(/[a-z]+/)) {
+            checkstrength += 1;
+        }
+        if (pwd.match(/[A-Z]+/)) {
+            checkstrength += 1;
+        }
+        if (pwd.match(/[0-9]+/)) {
+            checkstrength += 1;
+        }
+        if (pwd.match(/[$@#&!]+/)) {
+            checkstrength += 1;
+        }
+        switch (checkstrength) {
+            case 0:
+                $("#progressbar").css("width", "0%");
+                $("#progressbar").removeClass("bg-success");
+                $("#progressbar").removeClass("bg-danger");
+                $("#progressbar").removeClass("bg-warning");
+                break;
+            case 1:
+                $("#progressbar").css("width", "25%");
+                $("#progressbar").removeClass("bg-success");
+                $("#progressbar").text("25%");
+                $("#progressbar").addClass("bg-danger");
+                $("#progressbar").removeClass("bg-warning");
+                break;
+            case 2:
+                $("#progressbar").css("width", "50%");
+                $("#progressbar").removeClass("bg-success");
+                $("#progressbar").text("50%");
+                $("#progressbar").removeClass("bg-danger");
+                $("#progressbar").addClass("bg-warning");
+                break;
+            case 3:
+                $("#progressbar").css("width", "75%");
+                $("#progressbar").removeClass("bg-success");
+                $("#progressbar").text("75%");
+                $("#progressbar").removeClass("bg-danger");
+                $("#progressbar").removeClass("bg-warning");
+                break;
+            case 4:
+                $("#progressbar").css({
+                    "width": "100%"
+                });
+                $("#progressbar").removeClass("bg-warning");
+                $("#progressbar").removeClass("bg-danger");
+                $("#progressbar").addClass("bg-success");
+                $("#progressbar").text("100%");
+                break;
+        }
+    }
+
+});
